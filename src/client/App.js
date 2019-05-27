82bd986570ad89d6aae9ec177dd796fdc21178f9
Ahmet Mert Sevinc <>
2019-05-27 14:46:34 +0300

    A react component to post and retrieve values

--- src/client/App.js
+++ src/client/App.js
@@ -1,8 +1,16 @@
+/* eslint-disable no-alert */
+/* eslint-disable react/no-string-refs */
 import React, { Component } from 'react';
 import './app.css';
 
 export default class App extends Component {
-  state = { username: null };
+  constructor(props) {
+    super(props);
+    // eslint-disable-next-line react/no-unused-state
+    this.state = { data: '' };
+    this.state_2 = { message: [] };
+    this.onSubmit = this.handleSubmit.bind(this);
+  }
 
   componentDidMount() {
     fetch('/api/getUsername')
@@ -10,8 +18,46 @@ export default class App extends Component {
       .then(user => this.setState({ username: user.username }));
   }
 
+  handleSubmit(e) {
+    e.preventDefault();
+    const self = this;
+    // On submit of the form, send a POST request with the data to the server.
+    fetch('/api/calculatePrimes', {
+      method: 'POST',
+      body: JSON.stringify({
+        userValue: self.refs.userInput.value
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(response => response.json())
+      .then((body) => {
+        console.log(body);
+        if (body.error === true) {
+          alert(body.message);
+        } else {
+          // eslint-disable-next-line react/no-unused-state
+          this.setState({ primes: body.message });
+        }
+      });
+  }
+
   render() {
     const { username } = this.state;
-    return <div>{username ? <h2>{`Hello ${username}`}</h2> : <h2>Hang on</h2>}</div>;
+    const { primes } = this.state;
+    return (
+      <div id="parent">
+        <div>{username ? <h2>{`Hello ${username}`}</h2> : <h2>Hang on</h2>}</div>
+        <div>
+          <p>Please enter the number to see its factors:</p>
+        </div>
+        <div>
+          <form onSubmit={this.onSubmit}>
+            <input type="number" placeholder="Number to factorize" ref="userInput" />
+            <input type="submit" />
+          </form>
+        </div>
+        <div>{primes ? <h3>{`Here are the primes: ${primes}`}</h3> : <h3>Hang on</h3>}</div>
+      </div>
+    );
   }
 }
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const os = require('os');
+const bodyParser = require('body-parser');
 
 const app = express();
-
+app.use(bodyParser.json());
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => res.send({ username: os.userInfo().username }));
+
+app.post('/api/calculatePrimes', (req, res) => {
+  const postBody = req.body;
+  console.log(postBody.userValue);
+  if (!postBody.userValue) {
+    return res.send({ error: true, message: 'Please provide an input' });
+  }
+
+  const userValue = Number(postBody.userValue);
+  if (userValue <= 0) {
+    return res.send({ error: true, message: 'Please provide a valid number' });
+  }
+
+  return res.send({ error: false, message: userValue });
+});
 
 app.listen(process.env.PORT || 8080, () => console.log(`Listening on port ${process.env.PORT || 8080}!`));
